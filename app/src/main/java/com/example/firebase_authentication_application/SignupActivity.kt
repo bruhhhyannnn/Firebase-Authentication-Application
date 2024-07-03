9c7fdeb87e3a632e66a67d7ebb16deb9d4fed2e7
bruhhhyannnn <bryanjesusmangapit477@gmail.com>
2024-07-03 09:36:27 +0800

    added backend createAccountWithFirebase() functionality, changed app name

--- app/src/main/java/com/example/firebase_authentication_application/SignupActivity.kt
+++ app/src/main/java/com/example/firebase_authentication_application/SignupActivity.kt
@@ -1,8 +1,12 @@
 package com.example.firebase_authentication_application
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.firebase_authentication_application.databinding.ActivitySignupBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class SignupActivity : AppCompatActivity() {
 
@@ -13,6 +17,55 @@ class SignupActivity : AppCompatActivity() {
         binding = ActivitySignupBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.signupButton.setOnClickListener {
+            getData()
+        }
+        binding.signinActivity.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
 
+    private fun getData() {
+        val name = binding.nameEditText.text.toString()
+        val address = binding.addressEditText.text.toString()
+        val emailAddress = binding.emailAddressEditText.text.toString().trim()
+        val password = binding.passwordEditText.text.toString().trim()
+        val confirmPassword = binding.confirmPasswordEditText.text.toString().trim()
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(emailAddress).matches()) {
+            binding.emailAddressEditText.error = "Email is invalid"
+            return
+        }
+        if (password.isEmpty()) {
+            binding.passwordEditText.error = "Password is required"
+            return
+        }
+        if (password.length < 6) {
+            binding.passwordEditText.error = "Password must be 6 or above characters long"
+            return
+        }
+        if (password != confirmPassword) {
+            binding.passwordEditText.error = "Password doesn't match"
+            binding.confirmPasswordEditText.error = "Password doesn't match"
+            return
+        }
+
+        createAccountWithFirebase(name, address, emailAddress, password)
+    }
+
+    private fun createAccountWithFirebase(name: String, address: String, emailAddress: String, password: String) {
+        FirebaseAuth.getInstance()
+            .createUserWithEmailAndPassword(emailAddress, password)
+            .addOnSuccessListener {
+                Toast.makeText(this, "Account Created", Toast.LENGTH_SHORT).show()
+                // new activity
+                binding.passwordEditText.text.clear()
+                binding.confirmPasswordEditText.text.clear()
+            }
+            .addOnFailureListener {
+                Toast.makeText(this, "Something went wrong", Toast.LENGTH_SHORT).show()
+                binding.passwordEditText.text.clear()
+                binding.confirmPasswordEditText.text.clear()
+            }
     }
 }
